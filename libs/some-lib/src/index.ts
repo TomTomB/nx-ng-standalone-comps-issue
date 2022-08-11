6180db5dbd92084e430f43176c42d66256be7664
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2022-08-11 11:40:57 +0200

    Add as const a to angular docs

--- libs/some-lib/src/index.ts
+++ libs/some-lib/src/index.ts
@@ -3,4 +3,4 @@ import { MyCompComponent } from './lib/my-comp.component';
 export * from './lib/some-lib.module';
 export * from './lib/my-comp.component';
 
-export const STANDALONE_COMPS = [MyCompComponent];
+export const STANDALONE_COMPS = [MyCompComponent] as const;
